711bd573743df1e60707d1e87b7b2e501abf4f86
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-28 14:50:21 +0100

    Import and use JobListing component in JobListings

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,4 +1,5 @@
 <script setup>
+import JobListing from '@/components/JobListing.vue'
 import jobData from '@/jobs.json'
 import { ref } from 'vue'
 
@@ -6,7 +7,14 @@ const jobs = ref(jobData)
 </script>
 
 <template>
-  <div v-for="job in jobs" :key="job.id">
-    {{ job.title }}
-  </div>
+  <section class="bg-blue-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <div class="grid grid-cols-1 gap-6 md:grid-cols-3">
+        <JobListing v-for="job in jobs" :key="job.id" :job="job" />      
+      </div>
+    </div>
+  </section>
 </template>
