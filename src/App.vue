2f355dabe84a26e1bdf779edc43bd7dc062043c7
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-28 11:10:42 +0100

    Add fetching data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 // Composition API
 import { ref } from 'vue'
+import { onMounted } from 'vue'
 
 const name = ref('John Doe')
 const status = ref('active')
@@ -27,6 +28,16 @@ const addTask = () => {
 const deleteTask = (index) => {
   tasks.value.splice(index, 1)
 }
+
+onMounted(async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await response.json()
+    tasks.value = data.map((task) => task.title)
+  } catch (error) {
+    console.error('Error fetching tasks')
+  }
+})
 </script>
 
 <template>
