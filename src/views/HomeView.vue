669c542641da7020e28b62564cd49853ddd841c0
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-31 21:45:03 +0100

    fix: create home page and use router view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,11 @@
-<template>
-  <main></main>
+<script setup>
+import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+import JobListings from '@/components/JobListings.vue'
+</script>
+
+<template>  
+  <Hero />
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
 </template>
