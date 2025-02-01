f00fce5d33a273b4bba60bbdbdeadb0bc5ea777d
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-01 16:26:24 +0100

    fix: grid on md screens

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -3,8 +3,8 @@
 <template>
   <section class="bg-green-50">
     <div class="container m-auto py-10 px-6">
-      <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-        <main>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <main class="md:col-span-2">
           <div
             class="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
           >
